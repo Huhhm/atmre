3613b4f702417adee186e0fc5db6367b40d90566
ndtrepo <ductai8181@gmail.com>
2021-12-28 11:37:55 +0700

    main

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
+import 'react-native-gesture-handler';
 import React, { Component } from "react";
+import {
+  View,
+  Text,
+  Dimensions,
+
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator,DrawerItems } from '@react-navigation/native-stack';
+import { createDrawerNavigator,DrawerContentScrollView } from '@react-navigation/drawer';
 
 import { Provider } from 'react-redux';
 import { combineReducers , createStore , applyMiddleware} from 'redux';
@@ -11,14 +19,18 @@ import LoginScreen from './src/screens/Login';
 import RegisterScreen from './src/screens/Register';
 import HomeScreen from "./src/screens/Home";
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const rootReducer = combineReducers({ loginReducer });
 const store = createStore(rootReducer);
 
 const MyStack = () => {
   return (
-    
+      
       <Stack.Navigator>
         <Stack.Screen name="Login"  component={LoginScreen} options={{headerShown: false}} />
         <Stack.Screen name="Register"  component={RegisterScreen} options={{headerShown: false}} />
@@ -28,12 +40,37 @@ const MyStack = () => {
   );
 };
 
+function CustomDrawerContent(props) {
+  const width = windowWidth * 0.3;
+
+  return (
+    <DrawerContentScrollView {...props}>
+      <View>
+      <Text>Hello111</Text>
+          
+      </View>
+    </DrawerContentScrollView>
+  );
+}
+
+function MyDrawer({ navigation }) {
+  return (
+    <>
+      <Drawer.Navigator
+        drawerContent={(props) => <CustomDrawerContent {...props} />}>
+        <Drawer.Screen name="Screen1" component={MyStack} />
+      </Drawer.Navigator>
+    </>
+  );
+}
+
+
 
 const App = () => {
   return (
       <Provider store={store}>
         <NavigationContainer>
-          <MyStack />
+          <MyDrawer />
         </NavigationContainer>
       </Provider>
    
